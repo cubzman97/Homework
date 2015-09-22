cb68f554c8ecdd7d32ffa1bdd56cd19343017f6c
Devyn Theis <dtheis2@illinois.edu>
2015-09-22 13:19:53 -0500

    First Commit
    This is my first commit for PS2

--- PS2/problem2.cpp
+++ PS2/problem2.cpp
@@ -47,7 +47,7 @@ using namespace std;
 int main() {
     for (int i = 0; i < 3; i++) {
         for (int j = 3; j > 0; j--) {
-            cout << pow(i, j) << endl;
+            cout << pow(static_cast<double>(i), j) << endl;
         }
     }
 }
